a15ffae21816125aed2139d5c693b33cf167c300
Jonathan Roy <gigah3rtz@hotmail.fr>
2020-05-23 03:57:38 -0400

    Added article, changed some styles, new images

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@
 =============================================================== */
 const hamburgerContainer = document.querySelector(".hamburgerMenuContainer");
 const hamburgerMenu = document.querySelector(".hamburgerMenu");
-const hidingNavBar = document.querySelector(".navBarList");
+const hidingNavBar = document.querySelector(".hidingNavBar ");
 const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const subListOpener = document.querySelectorAll(".navItem");
@@ -18,7 +18,7 @@ hamburgerMenu.addEventListener("click", () => {
 });
 
 window.addEventListener("click", function (event) {
-  if (event.target !== header && event.target !== hamburgerMenu) {
+  if (subListOpener.includes(event.target) && event.target !== hamburgerMenu) {
     hamburgerContainer.classList.remove("openNav");
     body.classList.remove("noScroll");
     hidingNavBar.classList.remove("activeMainBar");
@@ -58,7 +58,7 @@ const faders = document.querySelectorAll(".fadeIn");
 
 const options = {
   root: null,
-  threshold: 0.55,
+  threshold: 1,
 };
 
 const fadeInOnScroll = new IntersectionObserver((entries) => {
@@ -126,11 +126,17 @@ tl2.from(
 /* ============================================================
                        ANIMATION GSAP
 =============================================================== */
-/* Menu animation with GSAP */
 const menuSections = document.querySelectorAll(".menuPrincipalSection");
+const shopContainer = document.querySelectorAll(".shopLocationContainer");
+const blogContainer = document.querySelectorAll(".blogContainer");
 
-let tl3 = gsap.timeline({ default: { ease: "power2" } });
+let tl3 = gsap.timeline({ default: { ease: "Power2" } });
+let tl4 = gsap.timeline({ default: { ease: "Power2" } });
+let tl5 = gsap.timeline({ default: { ease: "Power2" } });
+let tl6 = gsap.timeline({ default: { ease: "Power2" } });
+let tl7 = gsap.timeline({ default: { ease: "Power2" } });
 
+/* Menu animation with GSAP */
 tl3.from(
   ".menuTitle",
   {
@@ -162,10 +168,6 @@ for (let i = 0; i < menuSections.length; i++) {
 }
 
 /* Location animation with GSAP */
-const shopContainer = document.querySelectorAll(".shopLocationContainer");
-
-let tl4 = gsap.timeline({ default: { ease: "power2" } });
-
 tl4.from(".locationTitle", { duration: 0.6, opacity: 0, y: 30 }, "0.6");
 tl4.from(".locationDescription", { duration: 0.6, opacity: 0, y: 20 }, "-=0.4");
 tl4.from(
@@ -175,14 +177,36 @@ tl4.from(
 );
 
 /* Contact animation with GSAP */
-let tl5 = gsap.timeline({ default: { ease: "power2" } });
-
 tl5.from(".contactTitle", { duration: 0.6, opacity: 0, y: 30 }, "0.6");
 tl5.from(".contactInformation", { duration: 0.6, opacity: 0, y: 20 }, "-=0.4");
 tl5.from(".contactQuestions", { duration: 0.5, opacity: 0, y: 20 }, "-=0.4");
 tl5.from(".contactSection", { duration: 0.5, opacity: 0, y: 30 }, "-=0.3");
 tl5.from(".contactSendButton", { duration: 0.5, opacity: 0, y: 15 }, "-=0.2");
 
+/* Blog animation with GSAP */
+tl6.from(".blogTitle", { duration: 0.6, opacity: 0, y: 30 }, "0.6");
+tl6.from(".blogDescription", { duration: 0.6, opacity: 0, y: 20 }, "-=0.4");
+tl6.from(".blogContainer", { duration: 0.7, opacity: 0, y: 20 }, "-=0.4");
+
+/* Article animation with GSAP */
+tl7.from(".article1MainPhoto", { duration: 0.6, opacity: 0, y: 20 }, "0.6");
+tl7.from(".article2MainPhoto", { duration: 0.6, opacity: 0, y: 20 }, "0.6");
+tl7.from(".article3MainPhoto", { duration: 0.6, opacity: 0, y: 20 }, "0.6");
+tl7.from(".article4MainPhoto", { duration: 0.6, opacity: 0, y: 20 }, "0.6");
+tl7.from(".articleDate", { duration: 0.6, opacity: 0, y: 20 }, "-=0.4");
+tl7.from(".articleTitle", { duration: 0.6, opacity: 0, y: 30 }, "-=0.3");
+tl7.from(
+  ".articleMainContainer",
+  { duration: 0.7, opacity: 0, y: 20 },
+  "-=0.4"
+);
+tl7.from(
+  ".articleMainContainer p",
+  { duration: 0.7, opacity: 0, y: 20 },
+  "-=0.7"
+);
+tl7.from(".articleSubtitle", { duration: 0.7, opacity: 0, y: 20 }, "-=0.7");
+tl7.from(".nextArticle", { duration: 0.7, opacity: 0, y: 20 }, "-=0.5");
 /* ============================================================
               CHANGE COFFEE PRICE WHIT THE WEIGHT
 =============================================================== */
