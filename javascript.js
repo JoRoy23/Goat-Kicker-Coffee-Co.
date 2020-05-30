a7e5ea64e749f55b7481592eb8b5005e2129ab00
Jonathan Roy <gigah3rtz@hotmail.fr>
2020-05-30 15:14:01 -0400

    Added Story and made a lot of style change

--- javascript.js
+++ javascript.js
@@ -56,12 +56,18 @@ window.addEventListener("scroll", () => {
                        HOME PAGE
 =============================================================== */
 /* Move the best seller container when the width of the window is bigger then 768px */
-window.addEventListener("resize", () => {
+$(document).ready(() => {
   if ($(window).width() >= 768) {
     $(".homeBestSellerDescriptionContainer").appendTo(".homeBackgroundImage");
-  } else {
-    $(".homeBestSellerDescriptionContainer").appendTo(".homePageContainer");
   }
+
+  window.addEventListener("resize", () => {
+    if ($(window).width() >= 768) {
+      $(".homeBestSellerDescriptionContainer").appendTo(".homeBackgroundImage");
+    } else {
+      $(".homeBestSellerDescriptionContainer").appendTo(".homePageContainer");
+    }
+  });
 });
 
 /* ============================================================
@@ -138,90 +144,6 @@ tl2.from(
   "+=0.1"
 );
 
-/* ============================================================
-                       ANIMATION GSAP
-=============================================================== */
-const menuSections = document.querySelectorAll(".menuPrincipalSection");
-const shopContainer = document.querySelectorAll(".shopLocationContainer");
-const blogContainer = document.querySelectorAll(".blogContainer");
-
-let tl3 = gsap.timeline({ default: { ease: "Power2" } });
-let tl4 = gsap.timeline({ default: { ease: "Power2" } });
-let tl5 = gsap.timeline({ default: { ease: "Power2" } });
-let tl6 = gsap.timeline({ default: { ease: "Power2" } });
-let tl7 = gsap.timeline({ default: { ease: "Power2" } });
-
-/* Menu animation with GSAP */
-tl3.from(
-  ".menuTitle",
-  {
-    duration: 0.6,
-    opacity: 0,
-    y: 30,
-  },
-  "0.6"
-);
-tl3.from(
-  ".menuContainer",
-  {
-    duration: 0.6,
-    opacity: 0,
-    y: 30,
-  },
-  "-=0.4"
-);
-for (let i = 0; i < menuSections.length; i++) {
-  tl3.from(
-    menuSections[i],
-    {
-      duration: 0.6,
-      opacity: 0,
-      y: 20,
-    },
-    "-=0.4"
-  );
-}
-
-/* Location animation with GSAP */
-tl4.from(".locationTitle", { duration: 0.6, opacity: 0, y: 30 }, "0.6");
-tl4.from(".locationDescription", { duration: 0.6, opacity: 0, y: 20 }, "-=0.4");
-tl4.from(
-  ".shopLocationContainer",
-  { duration: 0.7, opacity: 0, y: 20 },
-  "-=0.4"
-);
-
-/* Contact animation with GSAP */
-tl5.from(".contactTitle", { duration: 0.6, opacity: 0, y: 30 }, "0.6");
-tl5.from(".contactInformation", { duration: 0.6, opacity: 0, y: 20 }, "-=0.4");
-tl5.from(".contactQuestions", { duration: 0.5, opacity: 0, y: 20 }, "-=0.4");
-tl5.from(".contactSection", { duration: 0.5, opacity: 0, y: 30 }, "-=0.3");
-tl5.from(".contactSendButton", { duration: 0.5, opacity: 0, y: 15 }, "-=0.2");
-
-/* Blog animation with GSAP */
-tl6.from(".blogTitle", { duration: 0.6, opacity: 0, y: 30 }, "0.6");
-tl6.from(".blogDescription", { duration: 0.6, opacity: 0, y: 20 }, "-=0.4");
-tl6.from(".blogContainer", { duration: 0.7, opacity: 0, y: 20 }, "-=0.4");
-
-/* Article animation with GSAP */
-tl7.from(".article1MainPhoto", { duration: 0.6, opacity: 0, y: 20 }, "0.6");
-tl7.from(".article2MainPhoto", { duration: 0.6, opacity: 0, y: 20 }, "0.6");
-tl7.from(".article3MainPhoto", { duration: 0.6, opacity: 0, y: 20 }, "0.6");
-tl7.from(".article4MainPhoto", { duration: 0.6, opacity: 0, y: 20 }, "0.6");
-tl7.from(".articleDate", { duration: 0.6, opacity: 0, y: 20 }, "-=0.4");
-tl7.from(".articleTitle", { duration: 0.6, opacity: 0, y: 30 }, "-=0.3");
-tl7.from(
-  ".articleMainContainer",
-  { duration: 0.7, opacity: 0, y: 20 },
-  "-=0.4"
-);
-tl7.from(
-  ".articleMainContainer p",
-  { duration: 0.7, opacity: 0, y: 20 },
-  "-=0.7"
-);
-tl7.from(".articleSubtitle", { duration: 0.7, opacity: 0, y: 20 }, "-=0.7");
-tl7.from(".nextArticle", { duration: 0.7, opacity: 0, y: 20 }, "-=0.5");
 /* ============================================================
               CHANGE COFFEE PRICE WHIT THE WEIGHT
 =============================================================== */
